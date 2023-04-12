cc2a84ddc1c9de7a22ccd74c28456f7673e40d86
JackBrickell94 <128167871+JackBrickell94@users.noreply.github.com>
2023-04-12 12:37:28 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -24,3 +24,8 @@ names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+FinalData <- TidyData %>%
+    group_by(subject, activity) %>%
+    summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
